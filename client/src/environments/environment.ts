edc09cb91d0b9343c6316b97ae9ea01cc5e54626
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-07-23 09:53:14 +0200

    MOD: nginx.conf file location.

--- client/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: '', // For production, paths are relative to the domain of the frontend app.
-  authPath: '/Auth',
-  apiPath: '/api'
-};
